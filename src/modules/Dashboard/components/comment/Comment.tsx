d0ad4005fa92af0ce09f7da012e26b7f4cc7f83c
Roksolana Dutchak <dytchakro@gmail.com>
2023-12-15 14:55:12 +0200

    add basic upload function

--- src/modules/Dashboard/components/comment/Comment.tsx
+++ src/modules/Dashboard/components/comment/Comment.tsx
@@ -3,6 +3,7 @@ import { useDropzone } from 'react-dropzone';
 import styles from './Comment.module.scss';
 
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 const Comment = () => {
 	const [files, setFiles] = useState([]);
 	const onDrop = useCallback((acceptedFiles) => {
@@ -25,7 +26,13 @@ const Comment = () => {
 			reader.readAsArrayBuffer(file);
 		});
 	}, []);
-
+	const handleUpload = (event) => {
+		event.preventDefault();
+		const formData = new FormData();
+		console.log(files[0]);
+		formData.append('file', files[0]);
+		console.log(formData);
+	};
 	const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 	const fileList = files.map((file) => (
 		<li key={file.name}>
@@ -34,17 +41,20 @@ const Comment = () => {
 		</li>
 	));
 	return (
-		<Box className={styles.dropzoneContainer}>
-			<div {...getRootProps()}>
-				<input {...getInputProps()} />
-				{isDragActive ? (
-					<p>Drop the files here ...</p>
-				) : (
-					<p>Drag and drop some files here, or click to select files</p>
-				)}
-				<ul className={styles.imageList}>{fileList}</ul>
-			</div>
-		</Box>
+		<form onSubmit={handleUpload}>
+			<Box className={styles.dropzoneContainer}>
+				<div {...getRootProps()}>
+					<input {...getInputProps()} />
+					{isDragActive ? (
+						<p>Drop the files here ...</p>
+					) : (
+						<p>Drag and drop some files here, or click to select files</p>
+					)}
+					<ul className={styles.imageList}>{fileList}</ul>
+				</div>
+			</Box>
+			<Button type="submit">Submit</Button>
+		</form>
 	);
 };
 
